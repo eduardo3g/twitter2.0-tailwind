3d05f50824eda811a5d9eaa0065e7ed223df269d
eduardo3g <eduardosbrasil10@gmail.com>
2022-05-23 20:36:23 -0300

    feat: make the UI responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
       <Head>
         <title>Twitter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="grid grid-cols-9">
         <Sidebar />
 
         <Feed />
 
-        {/* Widgets */}
+        <Widgets />
       </main>
     </div>
   )
